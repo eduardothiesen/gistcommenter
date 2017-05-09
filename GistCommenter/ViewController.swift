f720f908be7262d79bec902ebf8b91a1e66e972b
eduardothiesen <eduardo.thiesen@gmail.com>
2017-05-09 20:25:54 -0300

    Initial Commit

---
+++ GistCommenter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GistCommenter
+//
+//  Created by Eduardo Thiesen on 09/05/17.
+//  Copyright © 2017 Eduardo Thiesen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
